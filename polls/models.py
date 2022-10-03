eaf868074a03f17cb72f1e4619d05672f119c935
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-10-03 18:42:24 +0300

    create the models to to polls application

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,10 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+class Questions(models.Model):
+    question_text=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published')
+class Choice(models.Model):
+    question=models.ForeignKey(Questions,on_delete=models.CASCADE)
+    choice_text=models.CharField(max_length=230)
+    votes=models.IntegerField(default=0)
