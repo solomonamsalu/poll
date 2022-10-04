c98c05b5bb85b99ce90c21366072228601018247
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-10-04 22:54:23 +0300

    add result function

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result',views.results, name= 'result')
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,4 +6,6 @@ from django.http import HttpResponse
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
-    
+def results(request,question_id):
+    response="you are looking at the result of %s." 
+    return HttpResponse(response % question_id)
