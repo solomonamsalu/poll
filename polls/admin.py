c874b84ef83ec9bac4766d29bc4c672a109b91f4
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-10-05 07:48:41 +0300

    enter html to view

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from polls.models import Questions,Choice
 
-# Register your models here.
+admin.site.register(Questions)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,18 @@
+from multiprocessing import context
 from re import S
 from this import s
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Questions
-
+from django.template import loader
 def index(request):
     latest_question_list=Questions.objects.order_by('pub_date')[:5]
-    output=','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # template=loader.get_template("polls/index.html")
+    context={
+        "latest_question_list":latest_question_list,
+    }
+    # return HttpResponse(template.render(context,request))
+    return  render (request, 'polls/index.html', context)
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 def results(request,question_id):
